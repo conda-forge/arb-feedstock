7b609bb3bf1c92d1215e1909e03441836756376d
Isuru Fernando <isuruf@gmail.com>
2020-01-02 23:17:27 +0530

    Use LDFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,8 @@ export ARB_TEST_MULTIPLIER=0.1;
 # CFLAGS are not appended
 export CFLAGS="$CFLAGS -funroll-loops -g"
 
+sed -i.bak 's/$(LIBS)/$(LIBS) $(LDFLAGS)/g' Makefile.subdirsU
+
 ./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-flint=$PREFIX
 make -j${CPU_COUNT}
 make install

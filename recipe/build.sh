89124afda8ee5f067b7ec7644bff8cf92b8ddb50
Isuru Fernando <isuruf@gmail.com>
2021-03-20 02:55:44 -0500

    set BUILD

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,12 +9,24 @@ export ARB_TEST_MULTIPLIER=0.1;
 # CFLAGS are not appended
 export CFLAGS="$CFLAGS -funroll-loops -g"
 
-# build system uses BUILD for HOST
-export BUILD=$HOST
-
 sed -i.bak 's/$(LIBS)/$(LDFLAGS) $(LIBS)/g' Makefile.subdirs
 
-./configure --prefix=$PREFIX --with-gmp=$PREFIX --with-mpfr=$PREFIX --with-flint=$PREFIX --disable-static
+if [[ "$target_platform" == *-64 ]]; then
+  export ARCH="x86_64"
+fi
+if [[ "$target_platform" == linux-* ]]; then
+  export FLINT_BUILD="$ARCH-Linux"
+elif [[ "$target_platform" == osx-* ]]; then
+  export FLINT_BUILD="$ARCH-Darwin"
+fi
+
+./configure \
+  --prefix=$PREFIX \
+  --with-gmp=$PREFIX \
+  --with-mpfr=$PREFIX \
+  --with-flint=$PREFIX \
+  --disable-static \
+  --build=$FLINT_BUILD
 make -j${CPU_COUNT}
 make install
 
